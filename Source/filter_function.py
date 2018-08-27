3a034ad2d42c2a1eafc5583f313e4b3ce127498a
Unperknown <wnsdud105@gmail.com>
2018-08-27 20:11:19 +0900

    Update filter_function.py

--- Source/filter_function.py
+++ Source/filter_function.py
@@ -1,4 +1,5 @@
 # filter(실행할 함수, 입력할 리스트)
+# filter 함수는 bool 값을 반환하는 함수에서 *참을 반환하는 리스트 요소*만을 모음
 
 # 1부터 100까지 중에서 짝수만을 모은 리스트 할당하기
 even_number = list(filter(lambda x: x % 2 == 0, list(range(1, 101))))
