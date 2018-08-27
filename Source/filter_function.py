03175f0bb0e9cec48d126a5be878c924f524eb60
unKNOWn <37747694+Unperknown@users.noreply.github.com>
2018-08-27 20:08:51 +0900

    Python is really fun!

---
+++ Source/filter_function.py
@@ -0,0 +1,6 @@
+# filter(실행할 함수, 입력할 리스트)
+
+# 1부터 100까지 중에서 짝수만을 모은 리스트 할당하기
+even_number = list(filter(lambda x: x % 2 == 0, list(range(1, 101))))
+
+# even_number = [2, 4, 6, 8, 10, 12, 14, 16, 18, ..., 98, 100]
--- Source/lambda.py
+++ Source/lambda.py
@@ -1,3 +1,5 @@
+# (lambda 매개변수 인자들: 리턴할 표현식)(전달인자들)
+
 sentence = ["readability counts", "you only live once"]
 
 for word in sentence:
